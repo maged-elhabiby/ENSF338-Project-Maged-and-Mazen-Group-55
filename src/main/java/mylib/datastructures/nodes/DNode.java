fd8bc20b50f496ab47ae3fbabffbade3f66c484a
maged-elhabiby <magedelhabiby@gmail.com>
2023-04-07 16:53:27 -0600

    Created Dnode

--- src/main/java/mylib/datastructures/nodes/DNode.java
+++ src/main/java/mylib/datastructures/nodes/DNode.java
@@ -1,5 +1,29 @@
 package main.java.mylib.datastructures.nodes;
 
-public class DNode {
-    
+public class DNode<T> {
+    private T data;
+    private DNode<T> prev;
+    private DNode<T> next;
+
+    public DNode(T data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+    public T getData(){
+        return this.data;
+    }
+    public DNode<T> getPrev(){
+        return this.prev;
+    }
+    public void setPrev(DNode<T> prev){
+        this.prev = prev;
+    }
+    public DNode<T> getNext(){
+        return this.next;
+    }
+    public void setNext(DNode<T> next){
+        this.next = next;
+    }
+
 }
