025513cccd465c6d60812d1df05cf24b34697d2e
mazen-elhabiby <113324788+mazen-elhabiby@users.noreply.github.com>
2023-04-09 02:37:18 -0600

    Created SNode and DNode Tester

--- src/test/SNodeTest.java
+++ src/test/SNodeTest.java
@@ -1,5 +1,39 @@
 package test;
 
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+import main.java.mylib.datastructures.nodes.SNode;
+
 public class SNodeTest {
-    
+    SNode node;
+
+    @Before
+    public void setUp() {
+        node = new SNode(10);
+    }
+
+    @Test
+    public void testGetData() {
+        assertEquals(10, node.getData());
+    }
+
+    @Test
+    public void testSetData() {
+        node.setData(20);
+        assertEquals(20, node.getData());
+    }
+
+    @Test
+    public void testGetNext() {
+        assertNull(node.getNext());
+    }
+
+    @Test
+    public void testSetNext() {
+        SNode nextNode = new SNode(15);
+        node.setNext(nextNode);
+        assertEquals(nextNode, node.getNext());
+    }
 }
